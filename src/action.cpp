c94cd74a6c694f78f5d89d6b9f028f2afcf6c3e0
teesh3rt <ilaylevy611@gmail.com>
2025-10-08 20:53:57 +0300

    fix: fixups for actions

--- src/action.cpp
+++ src/action.cpp
@@ -1,17 +1,17 @@
 #include "action.hpp"
 #include <spdlog/spdlog.h>
 
+/*
+    An empty action is just a noop.
+    If you're gonna make an action, PLEASE inherit with public.
+*/
+
 Action::Action()
+    : name("EMPTY ACTION, PLEASE CHANGE"),
+      description("DONT USE THIS ACTION")
 {
-    spdlog::warn("You are constructing an empty action, which you probably shouldn't do!");
-    spdlog::debug("    Actions are meant to be inherited into their own classes instead of");
-    spdlog::debug("    using them like this. You should probably inherit Action into some");
-    spdlog::debug("    other class and make your action that way.");
 }
 
 void Action::affect(Fighter &affected)
 {
-    spdlog::warn("You are affectinge with an empty action, which does nothing!");
-    spdlog::debug("    You also probably shouldn't do it, as it quite literally changes");
-    spdlog::debug("    nothing about the fighter.");
 }
